599c9db9d78fc9486352b95555a79e9956196972
zhouXiaoBing <15527792708@163.com>
2018-05-24 11:50:52 +0800

    routine
    routine

---
+++ code/testc/testc/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  testc
+//
+//  Created by Mac on 2018/5/24.
+//  Copyright © 2018年 test. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
