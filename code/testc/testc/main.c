04601e3f19b6a16a613cc54315a0c159f6634f76
zhouXiaoBing <15527792708@163.com>
2018-06-03 22:47:24 +0800

    routine
    routine

--- code/testc/testc/main.c
+++ code/testc/testc/main.c
@@ -7,36 +7,38 @@
 //
 
 #include <stdio.h>
-
+union data{
+    long w;
+    float x;
+    int y;
+    char z;
+}beta;
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    
-    unsigned int x = 65535;
-    
+// insert code here...
+//    unsigned int x = 65535;
 //    printf("%d",x);
 //    printf("%d\n",x);
 //    printf("%d",x);
-    
 //    inws();
 //    nszxb();
-    struct{
-        int n;
-        int m;
-    }cnum[2]={1,2,3,4};
+//    struct{
+//        int n;
+//        int m;
+//    }cnum[2]={1,2,3,4};
 //    printf("%d\n",cnum[0].m/cnum[0].n*cnum[1].n);
 //    printf("%d\n",cnum[0].n);
 //    printf("%d\n",cnum[1].n);
 //    printf("%d\n",cnum[0].m);
 //    printf("%d\n",cnum[1].m);
 
-    union{
-         char c;
-        char b;
-        unsigned int i[4];
-    }z;
-    z.i[0] = 0x39;
-    z.i[1] = 0x36;
-    z.c = '9';
+//    union{
+//         char c;
+//        char b;
+//        unsigned int i[4];
+//    }z;
+//    z.i[0] = 0x39;
+//    z.i[1] = 0x36;
+//    z.c = '9';
     
 //    printf("%c\n",z.c);
 //    printf("%c\n",z.b);
@@ -44,22 +46,41 @@ int main(int argc, const char * argv[]) {
 //        printf("j %d\n",z.i[j]);
 //    }
     
-    char ch[3][4] = {"123","456","78"},*p[3];
-    int i;
-    for (i = 0; i<3; i++) {
-        p[i] = ch[i];
-//        printf("%c\n",ch[i][0]);
-    }
-    for (i = 0; i<3; i++) {
-//        printf("%d , %s\n",i,p[i]);
-    }
-    static int array[] = {0,1,2,3,4,5,6,7,8,9};
-    int j;
-    add1(array,10);
-    for (j = 0; j<10; j++) {
-        printf("%d",array[j]);
-    }
+//    char ch[3][4] = {"123","456","78"},*p[3];
+//    int i;
+//    for (i = 0; i<3; i++) {
+//        p[i] = ch[i];
+////        printf("%c\n",ch[i][0]);
+//    }
+//    for (i = 0; i<3; i++) {
+////        printf("%d , %s\n",i,p[i]);
+//    }
+//    static int array[] = {0,1,2,3,4,5,6,7,8,9};
+//    int j;
+//    add1(array,10);
+//    for (j = 0; j<10; j++) {
+//        printf("%d",array[j]);
+//    }
+    
+//    zxdata();
+    beta.w = 123456;
+    beta.y = 888;
+    beta.x = 3.14;
+    beta.z = 'x';
+    
+    printf("%d",beta.y);//x 的 ascii
     return 0;
+}
+
+
+
+void  zxdata(){
+    beta.w = 123456;
+    beta.y = 888;
+    beta.x = 3.14;
+    beta.z = 'x';
+    
+    printf("%c",beta.y);//x 的 ascii
 }
 
  int add1(int a[],int n){
