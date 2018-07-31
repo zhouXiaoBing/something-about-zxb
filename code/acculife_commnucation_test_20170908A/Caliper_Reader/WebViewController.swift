aad75e741ed9eb0386fcaa02e2ba67a777f618d3
zhouXiaoBing <15527792708@163.com>
2018-07-31 17:39:31 +0800

    routine
    routine

--- code/acculife_commnucation_test_20170908A/Caliper_Reader/WebViewController.swift
+++ code/acculife_commnucation_test_20170908A/Caliper_Reader/WebViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 import WebKit
-//import WKWebViewJavascriptBridge
+import WKWebViewJavascriptBridge
 
 class WebViewController : UIViewController {
 //    let webView = WKWebView(frame: CGRect(),configuration:WKWebViewConfiguration())
