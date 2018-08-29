c82c2facea0882fac6ca7b404f468ce6d6911d89
zhouXiaoBing <15527792708@163.com>
2018-08-29 08:54:40 +0800

    routine
    routine

---
+++ code/vitagou_v5/vitagou/vitagou/View/Home/HomeHorizontalCollectionView.h
@@ -0,0 +1,14 @@
+//
+//  HomeHorizontalCollectionView.h
+//  vitagou
+//
+//  Created by Mac on 2018/8/28.
+//  Copyright © 2018年 vitagou. All rights reserved.
+//
+
+@interface HomeHorizontalCollectionView : UICollectionViewCell
+
+
+@end
+
+
---
+++ code/vitagou_v5/vitagou/vitagou/View/Home/HomeHorizontalCollectionView.m
@@ -0,0 +1,24 @@
+
+//
+//  HomeHorizontalCollectionView.m
+//  vitagou
+//
+//  Created by Mac on 2018/8/28.
+//  Copyright © 2018年 vitagou. All rights reserved.
+//
+
+#import "HomeHorizontalCollectionView.h"
+
+@interface HomeHorizontalCollectionView()<UICollectionViewDelegate,UICollectionViewDataSource>
+
+@property (nonatomic,strong) UICollectionView *collectionView;
+
+
+@end
+
+@implementation HomeHorizontalCollectionView
+
+
+
+@end
+
