8ccd75b80d53474c7cfffb51f9db2d6155ba0629
zhouXiaoBing <15527792708@163.com>
2018-11-22 12:25:38 +0800

    routine
    routine

--- choiceness/Pods/Target Support Files/Pods-choiceness/Pods-choiceness-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_choiceness : NSObject
-@end
-@implementation PodsDummy_Pods_choiceness
-@end
--- code/cohoiceness/Pods/Target Support Files/Pods-cohoiceness/Pods-cohoiceness-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_cohoiceness : NSObject
-@end
-@implementation PodsDummy_Pods_cohoiceness
-@end
